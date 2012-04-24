0f8e9e0cf969024782d3bd1b474867a14399eabb
chris d <cgd39@cornell.edu>
2012-04-24 18:04:00 -0400

    Added WordCount example.

---
+++ src/WordCount/Reduce.java
@@ -0,0 +1,20 @@
+package WordCount;
+
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+	@Override
+	protected void reduce(Text key, java.lang.Iterable<IntWritable> values,
+			org.apache.hadoop.mapreduce.Reducer<Text, IntWritable, Text, 
+			IntWritable>.Context context)
+					throws IOException, InterruptedException {
+		int sum = 0;
+		for (IntWritable value : values) {
+			sum += value.get();
+		}
+		context.write(key, new IntWritable(sum));
+	}
+}
