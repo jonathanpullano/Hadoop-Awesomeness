4473c4c6c904e0656054d214db92f0a6795564c8
Jonathan Pullano <jp938@cornell.edu>
2012-04-24 18:45:01 -0400

    Tuples

---
+++ src/structures/Tuple.java
@@ -0,0 +1,37 @@
+package structures;
+
+import java.io.Serializable;
+
+public class Tuple implements Serializable, Comparable<Tuple>{
+    private int p;
+    private int q;
+    private int g;
+    
+    public Tuple(int p, int q) {
+        this.p = p;
+        this.q = q;
+    }
+    
+    public Tuple(int g, int p, int q) {
+        this(p,q);
+        this.g = g;
+    }
+    
+    public int getP() {
+        return p;
+    }
+
+    public int getQ() {
+        return q;
+    }
+
+    public int getG() {
+        return g;
+    }
+
+    @Override
+    public int compareTo(Tuple that) {
+        if(this == that) return 0;
+        return this.p - that.p;
+    }
+}
