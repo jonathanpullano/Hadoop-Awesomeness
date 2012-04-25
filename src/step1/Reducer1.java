095589c816db62aa1627ad2eb622b60bbbe3bd86
Jonathan Pullano <jp938@cornell.edu>
2012-04-25 16:52:29 -0400

    Fixed a few more longs

--- src/step1/Reducer1.java
+++ src/step1/Reducer1.java
@@ -3,25 +3,22 @@ package step1;
 import java.io.IOException;
 
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapreduce.Reducer;
 
+import structures.DisjointSet;
 import constants.Constants;
 
-import structures.DisjointSet;
-
-public class Reducer1 extends Reducer<IntWritable, LongWritable, IntWritable, LongWritable>{
+public class Reducer1 extends Reducer<IntWritable, IntWritable, IntWritable, IntWritable>{
 	@Override
 	/**
 	 * At the moment, this does nothing but emit what it's given
 	 */
-	protected void reduce(IntWritable key, java.lang.Iterable<LongWritable> values,
-			org.apache.hadoop.mapreduce.Reducer<IntWritable, LongWritable, IntWritable, 
-				LongWritable>.Context context)
+	protected void reduce(IntWritable key, Iterable<IntWritable> values,
+			Reducer<IntWritable, IntWritable, IntWritable, IntWritable>. Context context)
 					throws IOException, InterruptedException {
 	    DisjointSet set = new DisjointSet(Constants.groupLength * Constants.M);
-//		for (LongWritable value : values) {
-//			context.write(key,value);	
-//		}
+		for (IntWritable value : values) {
+			context.write(key,value);
+		}
 	}
 }
