0a93424604d1a196bf7d47e2f6592805fe3efad1
Jonathan Pullano <jp938@cornell.edu>
2012-04-25 14:52:39 -0400

    Updated Tuple

--- src/structures/Tuple.java
+++ src/structures/Tuple.java
@@ -1,38 +1,48 @@
 package structures;
 
-import java.io.Serializable;
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
 
-public class Tuple implements Serializable, Comparable<Tuple>{
-    private static final long serialVersionUID = -4348248931257442036L;
-    private int p;
-    private int q;
-    private int g;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.WritableComparable;
+
+public class Tuple implements WritableComparable<Tuple>{
+    private long p;
+    private long q;
     
-    public Tuple(int p, int q) {
+    public Tuple(long p, long q) {
         this.p = p;
         this.q = q;
     }
     
-    public Tuple(int g, int p, int q) {
-        this(p, q);
-        this.g = g;
-    }
-    
-    public int getP() {
+    public long getP() {
         return p;
     }
 
-    public int getQ() {
+    public long getQ() {
         return q;
-    }
-
-    public int getG() {
-        return g;
     }
 
     @Override
     public int compareTo(Tuple that) {
         if(this == that) return 0;
-        return this.p - that.p;
+        //Can't just return subtraction because of int overflow
+        long diff = this.p - that.p;
+        if(diff > 0) return 1;
+        if(diff < 0) return -1;
+        return 0;
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException {
+        p = in.readLong();
+        q = in.readLong();
+    }
+
+    @Override
+    public void write(DataOutput out) throws IOException {
+        out.writeLong(p);
+        out.writeLong(q);
     }
 }
