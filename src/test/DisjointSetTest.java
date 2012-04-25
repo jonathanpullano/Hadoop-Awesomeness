8a106542a28dfa0067dd1c1f72d4878a8085078b
Jonathan Pullano <jp938@cornell.edu>
2012-04-25 16:01:06 -0400

    Updated Union-Find to use p values

--- src/test/DisjointSetTest.java
+++ src/test/DisjointSetTest.java
@@ -11,38 +11,39 @@ public class DisjointSetTest {
     
     @Test(expected=IndexOutOfBoundsException.class)
     public void testIndexOutOfBoundsException() {
-        DisjointSet set = new DisjointSet(1,1);
-        set.isConnected(2,2,2,2);
+        DisjointSet set = new DisjointSet(1);
+        set.isConnected(1,1);
     }
     
     @Test(expected=IndexOutOfBoundsException.class)
     public void testIndexOutOfBoundsException2() {
-        DisjointSet set = new DisjointSet(0,0);
+        DisjointSet set = new DisjointSet(0);
     }
     
     @Test
     public void testConnected1() {
-        DisjointSet set = new DisjointSet(1,1);
-        assertTrue(set.isConnected(0,0,0,0));
+        DisjointSet set = new DisjointSet(1);
+        assertTrue(set.isConnected(0,0));
     }
     
     @Test
     public void testConnected2() {
-        DisjointSet set = new DisjointSet(2,2);
-        set.union(0, 0, 0, 1);
-        set.union(0, 0, 1, 0);
-        assertTrue(set.isConnected(0, 0, 0, 1));
-        assertTrue(set.isConnected(0, 0, 1, 0));
-        assertFalse(set.isConnected(0, 0, 1, 1));
+        DisjointSet set = new DisjointSet(4);
+        set.union(0, 1);
+        set.union(0, 2);
+        assertTrue(set.isConnected(0, 1));
+        assertTrue(set.isConnected(0, 2));
+        assertFalse(set.isConnected(0, 3));
+        assertFalse(set.isConnected(2, 3));
     }
     
     @Test
     public void testConnected3() {
-        DisjointSet set = new DisjointSet(3,3);
-        set.union(0, 0, 0, 1);
-        set.union(0, 1, 0, 2);
-        set.union(0, 2, 1, 2);
-        assertTrue(set.isConnected(0, 0, 1, 2));
-        assertFalse(set.isConnected(0, 1, 2, 0));
+        DisjointSet set = new DisjointSet(9);
+        set.union(0, 1);
+        set.union(1, 2);
+        set.union(2, 5);
+        assertTrue(set.isConnected(1, 5));
+        assertFalse(set.isConnected(7, 8));
     }
 }
