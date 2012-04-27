4af12408ce60bb9ffecad69e4b17ec637b6fc5c7
Abdelrahman Kamel <ak883@cornell.edu>
2012-04-27 15:59:02 -0400

    Adding small change to reducer

--- src/step2/Reducer2.java
+++ src/step2/Reducer2.java
@@ -1,7 +1,6 @@
 package step2;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Iterator;
 
 import org.apache.hadoop.io.IntWritable;
@@ -11,28 +10,35 @@ import structures.DisjointSet;
 import structures.Tuple;
 import constants.Constants;
 
-public class Reducer2 extends Reducer<IntWritable, Tuple, IntWritable, Tuple>{
-	
-	@Override
-	protected void reduce(IntWritable key, Iterable<Tuple> tuples,
-			Reducer<IntWritable, Tuple, IntWritable, Tuple>. Context context)
-					throws IOException, InterruptedException {
-	    DisjointSet set = new DisjointSet((2*Constants.g - 2)*Constants.M);
-	    
-	    Iterator<Tuple> it = tuples.iterator();
-	    Tuple prev = null;
-	    
-	    for (;it.hasNext();) {
-	    	Tuple tuple = it.next();
-	    	
-	    	System.out.println("Working with tuple (" + tuple.getP() + ", " + tuple.getQ() + ")");
-	    	
-	    	if(prev != null && prev.getP() == tuple.getP()){
-	    		set.union(tuple.getP(), tuple.getQ());
-	    		set.union(prev.getP(), prev.getQ());
-	    		context.write(key, new Tuple(tuple.getP(), set.find(tuple.getP())));
-	    	}	    	
-	    	prev = tuple;
-		}
-	}
+public class Reducer2 extends Reducer<IntWritable, Tuple, IntWritable, Tuple> {
+
+    @Override
+    protected void reduce(
+            final IntWritable key,
+            final Iterable<Tuple> tuples,
+            final Reducer<IntWritable, Tuple, IntWritable, Tuple>.Context context)
+            throws IOException, InterruptedException {
+        final DisjointSet set = new DisjointSet((2 * Constants.g - 2)
+                * Constants.M);
+
+        final Iterator<Tuple> it = tuples.iterator();
+        Tuple prev = null;
+
+        for (; it.hasNext();) {
+            final Tuple tuple = it.next();
+
+            System.out.println("Working with tuple (" + tuple.getP() + ", "
+                    + tuple.getQ() + ")");
+
+            if ((prev != null) && (prev.getP() == tuple.getP())) {
+                set.union(tuple.getP(), tuple.getQ());
+                set.union(prev.getP(), prev.getQ());
+                context.write(key,
+                        new Tuple(tuple.getP(), set.find(tuple.getP())));
+                context.write(key,
+                        new Tuple(prev.getP(), set.find(prev.getP())));
+            }
+            prev = tuple;
+        }
+    }
 }
--- src/step2/Step2.java
+++ src/step2/Step2.java
@@ -11,25 +11,25 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 import structures.Tuple;
 
-public class Step2{
-	public static void main(String[] args) throws Exception {
-		Configuration conf = new Configuration();
+public class Step2 {
+    public static void main(final String[] args) throws Exception {
+        final Configuration conf = new Configuration();
 
-		Job job = new Job(conf, "Step2");
-		job.setJarByClass(Step2.class);
+        final Job job = new Job(conf, "Step2");
+        job.setJarByClass(Step2.class);
 
-		job.setOutputKeyClass(IntWritable.class);
-		job.setOutputValueClass(Tuple.class);
+        job.setOutputKeyClass(IntWritable.class);
+        job.setOutputValueClass(Tuple.class);
 
-		job.setMapperClass(MapTest.class);
-		job.setReducerClass(Reducer2.class);
+        job.setMapperClass(MapTest.class);
+        job.setReducerClass(Reducer2.class);
 
-		job.setInputFormatClass(TextInputFormat.class);
-		job.setOutputFormatClass(TextOutputFormat.class);
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
 
-		FileInputFormat.addInputPath(job, new Path("data/input.txt"));
-		FileOutputFormat.setOutputPath(job, new Path("data/output/"));
+        FileInputFormat.addInputPath(job, new Path("data/input.txt"));
+        FileOutputFormat.setOutputPath(job, new Path("data/output/"));
 
-		job.waitForCompletion(true);
-	}
+        job.waitForCompletion(true);
+    }
 }
