8ecaa74bd5c2294262075e2474d2279f94b98730
Jonathan Pullano <jp938@cornell.edu>
2012-04-27 20:52:53 -0400

    Fixed merge conflicts

--- src/step1/Step1.java
+++ src/step1/Step1.java
@@ -4,15 +4,18 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.TestMapReduceLazyOutput.TestReducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 import structures.IntComparator;
+import test.Util;
 
 public class Step1 {
-    public static void main(final String[] args) throws Exception {
+    
+    public void run(String inputPath) throws Exception {
         final Configuration conf = new Configuration();
 
         final Job job = new Job(conf, "Step1");
@@ -23,16 +26,23 @@ public class Step1 {
 
         job.setMapperClass(Map1.class);
         //job.setReducerClass(Reducer1.class);
-
+        job.setReducerClass(TestReducer.class);
+        
         job.setSortComparatorClass(IntComparator.class);
         
         job.setInputFormatClass(TextInputFormat.class);
         job.setOutputFormatClass(TextOutputFormat.class);
 
         FileInputFormat
-                .addInputPath(job, new Path("data/data5.txt"));
+                .addInputPath(job, new Path(inputPath));
+        Util.deleteDir("data/output/");
         FileOutputFormat.setOutputPath(job, new Path("data/output/"));
 
         job.waitForCompletion(true);
+    }
+    
+    public static void main(final String[] args) throws Exception {
+        Step1 step = new Step1();
+        step.run("data/step1/test1.txt");
     }
 }
