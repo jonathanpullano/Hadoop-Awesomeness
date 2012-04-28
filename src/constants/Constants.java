a481765bde2dd0c1f161dfb0b6893893ba4fa128
Abdelrahman Kamel <ak883@cornell.edu>
2012-04-28 13:48:23 -0400

    Emitting twice in reducer2

--- src/constants/Constants.java
+++ src/constants/Constants.java
@@ -3,14 +3,19 @@ package constants;
 public class Constants {
 
     // The production file has 400,000,000 lines, which will fit in an int
-    public static int N = 16; // Total size of the file (probably should be
-                              // read)
+
+    // Total size of the file (probably should be read)
+    public static int N = 16;
+
     public static int M = (int) Math.sqrt(N);
-    public static int g = 2; // Number of Column Groups
-    public static int groupLength = M / g; // Length of a ColumnGroup (not
-                                           // including boundaries)
-    public static int groupSize = groupLength * M; // Number of elements in a
-                                                   // column group
+
+    // Number of Column Groups
+    public static int g = 2;
+    // Length of a ColumnGroup (not including boundaries)
+    public static int groupLength = M / g;
+
+    // Number of elements in a column group
+    public static int groupSize = groupLength * M;
 
     // compute filter parameters for netid ak883
     public static float fromNetID = 0.388f;
