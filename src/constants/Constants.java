3050a3edb7566f988dc86a311ae1dbade5526788
Jonathan Pullano <jp938@cornell.edu>
2012-04-28 14:41:35 -0400

    Made constants final; added debug

--- src/constants/Constants.java
+++ src/constants/Constants.java
@@ -3,18 +3,20 @@ package constants;
 public class Constants {
 
     // The production file has 400,000,000 lines, which will fit in an int
-    public static int N = 16; // Total size of the file (probably should be
+    public final static int N = 16; // Total size of the file (probably should be
                               // read)
-    public static int M = (int) Math.sqrt(N);
-    public static int g = 2; // Number of Column Groups
-    public static int groupLength = M / g; // Length of a ColumnGroup (not
+    public final static int M = (int) Math.sqrt(N);
+    public final static int g = 2; // Number of Column Groups
+    public final static int groupLength = M / g; // Length of a ColumnGroup (not
                                            // including boundaries)
-    public static int groupSize = groupLength * M; // Number of elements in a
+    public final static int groupSize = groupLength * M; // Number of elements in a
                                                    // column group
 
     // compute filter parameters for netid ak883
-    public static float fromNetID = 0.388f;
-    public static float desiredDensity = 0.59f;
-    public static float wMin = 0.4f * fromNetID;
-    public static float wLimit = wMin + desiredDensity;
+    public final static float fromNetID = 0.388f;
+    public final static float desiredDensity = 0.59f;
+    public final static float wMin = 0.4f * fromNetID;
+    public final static float wLimit = wMin + desiredDensity;
+    
+    public final static boolean DEBUG = true;
 }
