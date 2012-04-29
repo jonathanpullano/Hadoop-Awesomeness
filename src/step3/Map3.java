de605ec3c065fc144e1b56567ef5fcf6bb1c0e73
Abdelrahman Kamel <ak883@cornell.edu>
2012-04-29 15:36:07 -0400

    Adding mapper3 needs more unit test cases though

--- src/step3/Map3.java
+++ src/step3/Map3.java
@@ -3,8 +3,6 @@ package step3;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
@@ -21,33 +19,19 @@ public class Map3 extends Mapper<LongWritable, Text, IntWritable, Tuple> {
 
     private final Text red2Word_p = new Text();
     private final Text red2Word_q = new Text();
-    private static Queue<Tuple> queue = null;;
+    // private static Queue<Tuple> queue = null;
+    private int p, g, q;
+    Scanner scanner = null;
 
-    private static Queue<Tuple> initQueue() {
-        if ((queue != null) && !queue.isEmpty()) return queue;
-        final Queue<Tuple> tmpQueue = new LinkedList<Tuple>();
-        try {
-            final Scanner scanner = new Scanner(new File(
-                    "data/step3/input/red1Output.txt"));
-            int p, g;
-            while (scanner.hasNext()) {
-                p = scanner.nextInt();
-                g = scanner.nextInt();
-                scanner.nextInt(); // <-- Skips of q values in file
-
-                final Tuple tuple = new Tuple(p, g);
-                tmpQueue.offer(tuple);
-            }
-        } catch (final FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        return tmpQueue;
+    private void init() throws FileNotFoundException {
+        if (scanner != null) return;
+        scanner = new Scanner(new File("data/step3/input/red1Output.txt"));
     }
 
     @Override
     public void map(final LongWritable key, final Text value,
             final Context context) throws IOException, InterruptedException {
-        queue = initQueue();
+        init();
         final String line = value.toString();
         final StringTokenizer tokenizer = new StringTokenizer(line);
         int red_p, red_q, red_g;
@@ -58,19 +42,22 @@ public class Map3 extends Mapper<LongWritable, Text, IntWritable, Tuple> {
         red_q = Integer.parseInt(red2Word_q.toString());
         red_g = MatrixUtilities.getColumnGroup(Constants.M, Constants.g,
                 MatrixUtilities.getColumn(Constants.M, red_p));
-        while (!queue.isEmpty()) {
-            final Tuple curTup = queue.poll();
-            if ((curTup.getSecond() >= red_g) && (curTup.getFirst() >= red_p)) {
-                context.write(new IntWritable(curTup.getSecond()), new Tuple(
-                        curTup.getFirst(), curTup.getFirst()));
-                break;
-            }
 
-            if ((curTup.getSecond() <= red_g) && (curTup.getFirst() <= red_p))
-                context.write(new IntWritable(curTup.getSecond()), new Tuple(
-                        curTup.getFirst(), curTup.getFirst()));
+        while (scanner.hasNext()) {
+            p = scanner.nextInt();
+            g = scanner.nextInt();
+            q = scanner.nextInt(); // <- value never gets used...
+            context.write(new IntWritable(g), new Tuple(p, p));
+            if ((g >= red_g) && (p >= red_p)) break;
         }
 
         context.write(new IntWritable(red_g), new Tuple(red_p, red_q));
+
+        while (scanner.hasNext()) {
+            p = scanner.nextInt();
+            g = scanner.nextInt();
+            q = scanner.nextInt(); // <- value never gets used...
+            context.write(new IntWritable(g), new Tuple(p, p));
+        }
     }
 }
