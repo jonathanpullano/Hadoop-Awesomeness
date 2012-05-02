952a3295f1251bb3701213f3bf82c456b279ab88
Abdelrahman Kamel <ak883@cornell.edu>
2012-05-02 15:32:28 -0400

    Adding more test cases in base class

--- src/test/BaseTest.java
+++ src/test/BaseTest.java
@@ -10,22 +10,35 @@ import constants.Constants;
 public class BaseTest {
     private final BaseClass base = new BaseClass();
 
-    public BaseTest() {
+    @Test
+    public void testRun() throws Exception {
         // This code is evil
         Constants.N = 36;
         Constants.M = (int) Math.sqrt(Constants.N);
-        Constants.g = 3;
+        Constants.g = 2;
         Constants.numGroups = Constants.M / Constants.g;
         Constants.groupSize = Constants.M * (Constants.g + 1);
         Constants.wLimit = 1f;
         Constants.wMin = 0.1f;
         Constants.DEBUG = true;
-    }
-
-    @Test
-    public void testRun() throws Exception {
         base.start("data/appendix_test_files/data6-36.txt");
         assertTrue(Util.checkOutput(Constants.reducer5OutputDir
                 + "part-r-00000", "data/base/ans-36.txt"));
+    }
+
+    @Test
+    public void testSean() throws Exception {
+        Constants.N = 81;
+        Constants.M = (int) Math.sqrt(Constants.N);
+        Constants.g = 3;
+        Constants.numGroups = Constants.M / Constants.g;
+        Constants.groupSize = Constants.M * (Constants.g + 1);
+        Constants.wLimit = 1.25f;
+        Constants.wMin = .75f;
+        Constants.DEBUG = true;
+
+        base.start("data/step1/test-sean.txt");
+        assertTrue(Util.checkOutput(Constants.reducer3OutputDir
+                + "/part-r-00000", "data/step1/ans-sean.txt"));
     }
 }
