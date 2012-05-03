bdc21268b3ecbb1e275d92884ff8bcf801afebe7
Jonathan Pullano <jp938@cornell.edu>
2012-05-03 00:52:27 -0400

    Updated BaseTest

--- src/test/BaseTest.java
+++ src/test/BaseTest.java
@@ -10,18 +10,20 @@ import constants.Constants;
 public class BaseTest {
     private final BaseClass base = new BaseClass();
 
-//    @Test
-//    public void test36() throws Exception {
-//        // This code is evil
-//        Constants.N = 36;
-//        Constants.g = 2;
-//        Constants.wLimit = 1f;
-//        Constants.wMin = 0.1f;
-//        Constants.DEBUG = true;
-//        base.start("data/appendix_test_files/data6-36.txt");
-//        assertTrue(Util.checkOutput(Constants.reducer5OutputDir
-//                + "part-r-00000", "data/base/ans-36.txt"));
-//    }
+    @Test
+    public void test36() throws Exception {
+        // This code is evil
+        Constants.N = 36;
+        Constants.g = 2;
+        Constants.numGroups = Constants.M / Constants.g;
+        Constants.groupSize = Constants.M * (Constants.g + 1);
+        Constants.wLimit = 1f;
+        Constants.wMin = 0.1f;
+        Constants.DEBUG = true;
+        base.start("data/appendix_test_files/data6-36.txt");
+        assertTrue(Util.checkOutput(Constants.reducer5OutputDir
+                + "part-r-00000", "data/base/ans-36.txt"));
+    }
 //
 //    @Test
 //    public void testIsolation() throws Exception {
@@ -35,18 +37,18 @@ public class BaseTest {
 //                + "part-r-00000", "data/base/ans2.txt"));
 //    }
 //
-    @Test
-    public void testFull() throws Exception {
-
-        Constants.N = 1600;
-        Constants.g = 10;
-        Constants.wLimit = 1f;
-        Constants.wMin = 0.1f;
-        Constants.DEBUG = true;
-        base.start("data/appendix_test_files/data3.txt");
-        assertTrue(Util.checkOutput(Constants.reducer5OutputDir
-                + "part-r-00000", "data/base/ans3.txt"));
-    }
+//    @Test
+//    public void testFull() throws Exception {
+//
+//        Constants.N = 1600;
+//        Constants.g = 10;
+//        Constants.wLimit = 1f;
+//        Constants.wMin = 0.1f;
+//        Constants.DEBUG = true;
+//        base.start("data/appendix_test_files/data3.txt");
+//        assertTrue(Util.checkOutput(Constants.reducer5OutputDir
+//                + "part-r-00000", "data/base/ans3.txt"));
+//    }
 
 //
 //    @Test
